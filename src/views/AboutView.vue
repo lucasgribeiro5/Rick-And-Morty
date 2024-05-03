76def2a8a72852bca394e1589e4f3cd647db5613
Lucas Ribeiro <lucasgarciaribeiro05@gmail.com>
2024-05-03 16:25:48 -0300

    sdas

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>
-    Essa é uma pagina teste
-  </div>
+
+  <center>
+    <div class="card card_a">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="../assets/perfil.jpg" class="card-img" style="width: 25vh; margin-left: 10px; border-radius: 12px;" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card_a-body" style="margin-left: 3vh">
+            <h5 class="card-title">Lucas Garcia Ribeiro</h5>
+            <ul style="margin-top:4.5vh;"></ul>
+            <p> 19 anos
+              <br> Atualmente estou cursando Análise e Desenvolvimento de Sistemas(ADS) e fazendo estágio na UNIMAR,
+              <br> estou no 3° TERMO - 3/6.
+            </p>
+            <p><b>CONTATO:</b> (14) 996770520</p>
+            <p><b>E-MAIL:</b> lucasgarciaribeiro05@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </center>
+
+
+
 </template>
 
 <style>
-  
+body {
+  background-color: #081119;
+}
+
+.card_a {
+  width: 680px;
+  height: 350px;
+  background-color: #ffffff;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+  border-radius: 20px;
+}
 </style>
